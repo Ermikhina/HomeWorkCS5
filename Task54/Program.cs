ff3da7e6c2e731078ef72eb05ff5c061a8e42736
Natal <nat_n_e@mail.ru>
2023-04-19 18:07:10 +0300

    Added program for Task54

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит
+// по убыванию элементы каждой строки двумерного массива. Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],6}");
+        }
+        Console.WriteLine();
+    }
+}
+
+void CreateMatrixSortedStrings(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1)-1; j++)
+        {
+            int indMax = j;
+            for (int k = j + 1; k < matrix.GetLength(1); k++)
+            {
+                if (matrix[i, indMax] < matrix[i, k]) indMax = k;
+            }
+            int temp = matrix[i, indMax];
+            matrix[i, indMax] = matrix[i, j];
+            matrix[i, j] = temp;
+        }
+    }
+}
+    int[,] matr = CreateMatrixRndInt(5, 4, 0, 100);
+    PrintMatrix(matr);
+    CreateMatrixSortedStrings(matr);
+    Console.WriteLine("Sorted strings:");
+    PrintMatrix(matr);
