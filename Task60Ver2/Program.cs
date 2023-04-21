ab01d0b1ad0497b20782fa48381f0dea97152ec8
Natal <nat_n_e@mail.ru>
2023-04-21 18:22:39 +0300

    Added program for Task60Ver2

--- Task60Ver2/Program.cs
+++ Task60Ver2/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+// Напишите программу, которая будет построчно выводить массив, добавляя 
+// индексы каждого элемента. Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+// Исключение возможности 3-хзначных чисел при размерности 3Dматрицы более 90 элементов
+
+
+int[,,] Create3DMatrix(int rows, int columns, int layers)
+{
+    int[,,] matrix3D = new int[rows, columns, layers];
+    int numb = 10;
+    for (int i = 0; i < matrix3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix3D.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix3D.GetLength(2); k++)
+            {
+                matrix3D[i, j, k] = numb;
+                numb++;
+                if (numb>99) numb = 10;
+            }
+        }
+    }
+    return matrix3D;
+}
+void PrintMatrix3D(int[,,] matrix3D)
+{
+    for (int i = 0; i < matrix3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix3D.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix3D.GetLength(2); k++)
+            {
+                Console.Write($"{matrix3D[i, j, k],6} ({i} ,{j} ,{k})");
+
+            }
+             Console.WriteLine();
+        }
+    }
+}
+int[,,] matr3D = Create3DMatrix(10, 10, 3);
+PrintMatrix3D(matr3D);
