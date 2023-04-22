0be0f40f15afa302800194b86b8f7edd6dee9b80
Natal <nat_n_e@mail.ru>
2023-04-22 20:54:47 +0300

    Changed program for Task66

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -12,7 +12,11 @@ Console.Write("Input natural number M: ");
 int numberM = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input natural number N: ");
 int numberN = Convert.ToInt32(Console.ReadLine());
-int sum = 0;
-if (numberM < numberN) sum = SumOfNumbers(numberM, numberN);
-else sum = SumOfNumbers(numberN, numberM);
-Console.WriteLine($"The sum of the numbers from M to N = {sum}");
+if (numberM > 0 && numberN > 0)
+{
+    int sum = 0;
+    if (numberM < numberN) sum = SumOfNumbers(numberM, numberN);
+    else sum = SumOfNumbers(numberN, numberM);
+    Console.WriteLine($"The sum of the numbers from M to N = {sum}");
+} 
+else Console.WriteLine("Input is not correct");
